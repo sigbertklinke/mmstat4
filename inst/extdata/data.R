1c5ba7248f9855a6ad89e09b69513a15764dd989
Sigbert Klinke <sigbert@wiwi.hu-berlin.de>
2023-01-31 14:55:47 +0100

    delete extdata/data.R

--- inst/extdata/data.R
+++
@@ -1,8 +0,0 @@
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-#
-library("rio")
-x <- import("GSS.SAV")
-vars <- c("age", "sex", "educ", "sibs", "life", "speduc", "paeduc", "maeduc", "tvhours", "wrkstat",
-          "hrs1", "incomdol", "rincmdol", "wifeduc", "husbeduc", "wifeft", "happy", "hapmar")
-gss <- x[,vars]
-save(gss, file = "../data/gss.rda", version=2)
